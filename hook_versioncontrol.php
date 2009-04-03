d1acc33ef3c5489f03106d19ec645cebfd2c2a1e
jpetso <jpetso>
2009-04-03 22:06:57 +0000

    #328035 by Cornil, plus a lot of changes from me: Make VCS username suggestions and determing username validity pluggable. Also, fix a bug in the user versioncontrol page that made the passed repository work incorrectly.
    This should make it possible to handle Git & Co. usernames properly.

--- hook_versioncontrol.php
+++ hook_versioncontrol.php
@@ -415,6 +415,12 @@ function hook_versioncontrol_alter_repository_selection(&$repository_names, $rep
  * Let the Version Control API know whether the given VCS account
  * is authorized or not.
  *
+ * @param $repository
+ *   The repository where the status should be checked. (Note that the user's
+ *   authorization status may differ for each repository.)
+ * @param $uid
+ *   The user id of the checked account.
+ *
  * @return
  *   TRUE if the account is authorized, or FALSE if it's not.
  *
@@ -422,7 +428,7 @@ function hook_versioncontrol_alter_repository_selection(&$repository_names, $rep
  * @ingroup Authorization
  * @ingroup Target audience: Authorization control modules
  */
-function hook_versioncontrol_is_account_authorized($uid, $repository) {
+function hook_versioncontrol_is_account_authorized($repository, $uid) {
   if ($repository['authorization_method'] != 'mymodule_dojo_status') {
     return TRUE;
   }
